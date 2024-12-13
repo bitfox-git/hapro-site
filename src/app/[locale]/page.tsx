d3fa4deef77f1dd15ccbffa7f6f08eb830387dec
Wouter Missler <woutmissler2@gmail.com>
2024-12-13 17:28:27 +0100

    internationalisation setup

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,25 @@
+import { useTranslations } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+
+type HomeProps = Readonly<{
+    params: {
+        locale: string;
+    };
+}>;
+
+export async function generateMetadata({ params: { locale } }: HomeProps) {
+    const t = await getTranslations({ locale, namespace: "metadata" });
+
+    return {
+        title: t("title"),
+    };
+}
+
+export default function Home({ params: { locale } }: HomeProps) {
+    // Enable static rendering
+    setRequestLocale(locale);
+
+    const t = useTranslations("home_page");
+
+    return <h1>{t("title")}</h1>;
+}
