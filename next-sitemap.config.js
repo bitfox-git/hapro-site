96a00d1f3a9d57651b9ac75b33b54e8fc66fee35
Wouter Missler <woutmissler2@gmail.com>
2024-12-18 12:58:21 +0100

    always robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || "https://hapro.cloud",
-    generateRobotsTxt: process.env.NODE_ENV === "production",
+    generateRobotsTxt: true,
 };
