6c825676ace8879ced65738185ab4ce05f50e477
Wouter Missler <woutmissler2@gmail.com>
2024-12-18 10:09:44 +0100

    faq hero done

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -1,8 +1,27 @@
+import TitleSubtitle from "@/components/type/TitleSubtitle";
+import styles from "./page.module.css";
+import { TextField } from "@radix-ui/themes";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+
 export default function FAQPage() {
     return (
-        <div>
-            <h1>FAQ</h1>
-            <p>Here are the answers to the most frequently asked questions.</p>
+        <div className={styles.container}>
+            <div className={styles.hero}>
+                <TitleSubtitle
+                    title="What can we help you find?"
+                    subtitle="FAQ"
+                    large
+                />
+                <TextField.Root
+                    placeholder="Search the faqs…"
+                    size={"3"}
+                    className={styles.searchBox}
+                >
+                    <TextField.Slot>
+                        <MagnifyingGlassIcon height="20" width="20" />
+                    </TextField.Slot>
+                </TextField.Root>
+            </div>
         </div>
     );
 }
