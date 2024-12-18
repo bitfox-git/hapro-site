adfa78bd53d6ef1b1095eb84690f7b1d9b78deff
Wouter Missler <woutmissler2@gmail.com>
2024-12-18 12:45:34 +0100

    robots txt only in prod

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || "https://hapro.cloud",
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-        transformRobotsTxt: (robotsTxt) => {
-            if (process.env.NODE_ENV === "development") {
-                return `User-agent: *
-                        Disallow: /`;
-            }
-
-            return robotsTxt;
-        },
-    },
-    // ...other options
+    generateRobotsTxt: process.env.NODE_ENV === "production",
 };
