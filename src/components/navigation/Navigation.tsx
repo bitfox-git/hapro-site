b0252fd96529e4fbbcd90c2e00ebdf040269da45
Wouter Missler <woutmissler2@gmail.com>
2025-01-15 15:51:37 +0100

    ok no no dark nav

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -73,7 +73,7 @@ export default function Navigation() {
                         >
                             {link.logo ? (
                                 <Image
-                                    src="/hapro-logo-full-dark.svg"
+                                    src="/hapro-logo-full.svg"
                                     alt="Logo"
                                     width={77}
                                     height={22}
