f206078be64e6343d6c1c18b4cedf6e8fff1d0f3
Wouter Missler <woutmissler2@gmail.com>
2025-01-15 15:49:12 +0100

    dark navbar

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -73,7 +73,7 @@ export default function Navigation() {
                         >
                             {link.logo ? (
                                 <Image
-                                    src="/hapro-logo-full.svg"
+                                    src="/hapro-logo-full-dark.svg"
                                     alt="Logo"
                                     width={77}
                                     height={22}
